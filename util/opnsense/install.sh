7b26f8cb3f52bfdc056368b18032474f5a8b7f16
Andrey "Limych" Khrolenok <andrey@khrolenok.ru>
2019-04-08 18:52:54 +0300

    Added shell version of deploy_freenas

--- util/opnsense/install.sh
+++ util/opnsense/install.sh
@@ -5,7 +5,10 @@ DIR=$(cd `dirname $0` && pwd)
 # Make service links
 ln -Fs /var/etc/acme-client/home/ /root/.acme.sh
 
-# Install action
+# Install required packages
+#pkg install -y bash
+
+# Install cron action
 ln -Fs ${DIR}/actions_deploy_freenas.conf /usr/local/opnsense/service/conf/actions.d/actions_deploy_freenas.conf
 
 # Reload config daemon
