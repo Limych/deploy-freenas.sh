d40cd74b27d3d28b001ba717a82816367e1e6754
Andrey "Limych" Khrolenok <andrey@khrolenok.ru>
2019-04-10 21:03:25 +0300

    Config file check

--- util/opnsense/install.sh
+++ util/opnsense/install.sh
@@ -7,3 +7,9 @@ ln -Fs ${WDIR}/actions_deploy_freenas.conf /usr/local/opnsense/service/conf/acti
 
 # Reload config daemon
 service configd restart
+
+# Make default config file if it not exists
+CONF="$(cd `dirname $WDIR/../..` && pwd)/deploy_config"
+if [ ! -e "$CONF" ]; then
+  echo "WARNING! Please be sure to write configuration parameters in file deploy_config"
+fi
